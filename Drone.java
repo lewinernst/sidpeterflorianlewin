47331245f12858c0c9fbc3cc8d742d299fded283
Florian Hahn <flo@fhahn.com>
2016-02-11 19:45:58 +0100

    Add drone stub

---
+++ Drone.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Drone {
+	public static int payload;
+	
+	public int x;
+	public int y;
+	
+	public List<Integer> loadedItems;
+}
--- Simulation.java
+++ Simulation.java
@@ -1,4 +1,10 @@
+import java.util.List;
 
 public class Simulation {
-
+	public int round;
+	
+	public List<Order> outstandingOrders;
+	
+	public List<Drone> availableDrones;
+	
 }
