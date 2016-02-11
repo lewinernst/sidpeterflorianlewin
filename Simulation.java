6f421bcc732dfb6d1777d9cb4281099e13bbf946
Florian Hahn <flo@fhahn.com>
2016-02-11 20:29:00 +0100

    Update again

--- Simulation.java
+++ Simulation.java
@@ -1,7 +1,8 @@
 import java.util.List;
+import java.util.Map;
 
 public class Simulation {
-	public int round;
+	public static int MAX_ROUNDS;
 	
 	public List<Order> outstandingOrders;
 	
@@ -14,19 +15,36 @@ public class Simulation {
 	public Items items;
 
 	public Order findOrder() {
-		for (Order o: outstandingOrders) {
-			for ()
-		}
+		Warehouse first = warehouses.get(0);
 		
+		for (Order o: outstandingOrders) {
+			boolean allOk = true;
+			for (Map.Entry<Integer, Integer> entry : o.items.entrySet()) {
+				if (!first.hasInStore(entry.getKey(), entry.getValue())) {
+					allOk = false;
+				}
+			}
+			if (allOk) {
+				return o;
+			}
+		}
+		return null;
 	}
 	
 	public void scheduleDrones() {
 		for (Drone d : availableDrones) {
-			
+			Order order = findOrder();
+			d.outstandingOrders.add(order);
+			outstandingOrders.remove(order);
+			workingDrones.add(d);
 		}
+		
+		availableDrones.removeAll(workingDrones);
 	}
 
 	public void run() {
-		
+		for (int i=0; i < MAX_ROUNDS; i++) {
+			scheduleDrones();			
+		}
 	}
 }
